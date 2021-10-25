341099dba269e6919ac3117eb38c302f04051bc7
Proevento <andre.ricardo2115@gmail.com>
2021-10-25 15:53:10 -0300

    Adicionando Primeira pagina

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,16 @@
+import { homedir } from "os";
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className = "titulo"> André </h1>
+            <p></p>
+            <img className = "imagens" src="https://avatars.githubusercontent.com/u/88911837?v=4" alt="Foto do andré" />
+        </>
+    );
+
+}
+
+export default Home;
